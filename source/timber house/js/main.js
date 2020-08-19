ff40995816f2e14cf6e180714361b19be3182482
Abuy004 <Abuy004@yandex.ru>
2020-08-19 16:51:27 +0300

    timber

---
+++ source/timber house/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.gallery__inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
+	});
+});
