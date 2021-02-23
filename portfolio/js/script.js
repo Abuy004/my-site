96e95a06f04b0584e033cc0f11831e671217ed0f
Abuy004 <Abuy004@yandex.ru>
2021-02-23 20:35:59 +0300

    portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,35 @@
+const hamburger = document.querySelector('.hamburger'),
+menu = document.querySelector('.menu'),
+closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.usage__skills-counter'),
+lines = document.querySelectorAll('.usage__skills-line span');
+
+counters.forEach( (item, i) => {
+lines[i].style.width = item.innerHTML;
+});
+    
+      $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
